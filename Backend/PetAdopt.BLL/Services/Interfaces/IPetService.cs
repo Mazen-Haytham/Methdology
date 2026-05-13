15a1014dd1c702b4cb8198c22201f02c3cdd2099
mazen <mazenhaytham26@gmail.com>
2026-05-14 00:03:35 +0300

    Add backend phase 2

---
+++ Backend/PetAdopt.BLL/Services/Interfaces/IPetService.cs
@@ -0,0 +1,33 @@
+using Azure;
+using PetAdopt.BLL.DTOs.Pet;
+using PetAdopt.DAL.Models;
+using PetAdopt.DAL.Pagination;
+using PetAdopt.DAL.Repositories.Implementations;
+
+namespace PetAdopt.BLL.Services.Interfaces
+{
+    public interface IPetService
+    {
+        Task<PagedResult<PetHomeResponseDto>> GetApprovedPetsAsync(PaginationParams param);
+
+
+        Task<PetResponseDto?> GetPetByIdAsync(int petId);
+        Task<PagedResult<PetHomeResponseDto>> SearchPetsAsync( PaginationParams paginationParams,
+            string? animalType,
+            string? breed,
+            int? maxAge,
+            string? location);
+
+        Task<PetResponseDto> CreatePetAsync(CreatePetDto dto, string ownerId);
+        Task<bool> UpdatePetAsync(int petId, UpdatePetDto dto, string ownerId);
+        Task<bool> DeletePetAsync(int petId, string ownerId);
+        Task<IEnumerable<PetResponseDto>> GetMyPetsAsync(string ownerId);
+
+        Task<IEnumerable<PetResponseDto>> GetPendingApprovalPetsAsync();
+        Task<bool> ApprovePetAsync(int petId);
+        Task<bool> RejectPetAsync(int petId);
+
+
+    }
+    
+}
